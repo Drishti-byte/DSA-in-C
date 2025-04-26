17d55332da032b9d46cf2baf3e1a81e5c30466cf
Drishti-byte <drishtibansal06@gmail.com>
2025-04-26 10:26:21 +0530

    Add files via upload

---
+++ tree.c
@@ -0,0 +1,106 @@
+/*program to implement binary search trees(BST) using linked list*/
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node 
+{
+    int data;
+    struct Node *left,*right;
+}Tree;
+Tree *root;
+void add(int num)
+{
+    Tree *ptr,*temp;
+    temp = (Tree*)malloc(sizeof(Tree));
+    temp->data = num;
+    temp->left = NULL;
+    temp->right = NULL;
+    if(root == NULL)
+    root = temp;
+    else 
+    {
+        ptr = root;
+        while(ptr != NULL)
+        {
+            if(num > ptr->data)
+            {
+                if(ptr->right == NULL)
+                {
+                    ptr->right = temp;
+                    break;
+                }
+                else 
+                ptr = ptr->right;
+            }
+            else if(num < ptr->data)
+            {
+                if(ptr->left == NULL)
+                {
+                    ptr->left = temp;
+                    break;
+                }
+                else 
+                ptr = ptr->left;
+            }
+            else 
+            {
+                printf("\nDuplicate value is not allowed");
+                free(temp);
+                break;
+            }
+        }
+    }
+}
+void preorder(Tree *ptr)
+{
+    if(ptr == NULL)
+    return;
+    printf("%d\t",ptr->data);
+    preorder(ptr->left);
+    preorder(ptr->right);
+}
+void inorder(Tree *ptr)
+{
+    if(ptr == NULL)
+    return;
+    inorder(ptr->left);
+    printf("%d\t",ptr->data);
+    inorder(ptr->right);
+}
+void postorder(Tree *ptr)
+{
+    if(ptr == NULL)
+    return;
+    postorder(ptr->left);
+    postorder(ptr->right);
+    printf("%d\t",ptr->data);
+}
+int main()
+{
+    root = NULL;
+    int ch,num;
+    do
+    {
+        printf("\nPress 1 - Add a new node");
+        printf("\nPress 2 - Display the traversal order");
+        printf("\nPress 3 - Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:
+            printf("\nEnter a number: ");
+            scanf("%d",&num);
+            add(num);
+            break;
+            case 2:
+            printf("\nPreorder traversal: ");
+            preorder(root);
+            printf("\nInorder traversal: ");
+            inorder(root);
+            printf("\nPostorder traversal: ");
+            postorder(root);
+            break;
+        }
+    } while (ch != 3);
+    return 0;
+}
